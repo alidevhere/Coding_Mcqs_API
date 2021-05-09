62a123de3997496a4eef1a4b5cb7ede3f77e4d92
mohammad ali <ranaaliashraf12@gmail.com>
2021-05-09 14:57:46 +0500

    changes database

--- coding_test_api/settings.py
+++ coding_test_api/settings.py
@@ -79,18 +79,18 @@ WSGI_APPLICATION = 'coding_test_api.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-'''
+#'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+#'''
+
+
+
 '''
-
-
-
-
 DATABASES = {
 
     'default': {
@@ -110,7 +110,7 @@ DATABASES = {
     }
 
 }
-
+'''
 
 
 
