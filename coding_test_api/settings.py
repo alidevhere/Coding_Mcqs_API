0ff3b7c7f16fc356d9995ce5c13ab5a866ff8b69
mohammad ali <ranaaliashraf12@gmail.com>
2021-05-09 05:50:07 +0500

    deploy

--- coding_test_api/settings.py
+++ coding_test_api/settings.py
@@ -14,8 +14,9 @@ from pathlib import Path
 import django_heroku
 import sys
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+#BASE_DIR = Path(__file__).resolve().parent.parent
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -157,6 +158,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
